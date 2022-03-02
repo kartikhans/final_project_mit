da0646e1b193a4d0153d03e6f5e8ade5272bfaf6
kartikhans <kartik.feb.hans@gmail.com>
2022-03-02 14:49:25 +0530

    add: push after testing

--- ecom_project/urls.py
+++ ecom_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
-    path('', include("accounts.urls")), 
-    path('accounts/', include("django.contrib.auth.urls")),   # working for login.html
+    path('login/', include("accounts.urls")),
+    path('accounts/', include("django.contrib.auth.urls")),  # working for login.html
 ]
